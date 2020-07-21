0a69b9882b73c9b00565f40167aeaeaff0956561
Syrus Dark <naveen@syrusdark.website>
2020-07-21 19:29:35 +0530

    Try to Install manim

--- python-x86.sh
+++ python-x86.sh
@@ -4,4 +4,5 @@ export FFMPEG_FILE=ffmpeg-4.3-win32-static
 export SOX_FILE=sox-14.4.2-win32
 ./pythonx86.$PYVER/tools/python.exe -m pip install -i https://test.pypi.org/simple/ pycairo
 ./pythonx86.$PYVER/tools/python.exe -m pip install -r manim/requirements.txt
+./pythonx86.$PYVER/tools/python.exe pip -m pip install .
 cd ../
