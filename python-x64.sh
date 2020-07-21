4d573c70d87309b53f7992959adaa79b1fad7c13
Syrus Dark <naveen@syrusdark.website>
2020-07-21 12:59:15 +0530

    Try for 64 bit

---
+++ python-x64.sh
@@ -0,0 +1,13 @@
+# build x64 python
+
+export FFMPEG_FILE=ffmpeg-4.3-win64-static
+export SOX_FILE=sox-14.4.2-win32
+./python.$PYVER/tools/python.exe -m pip install -i https://test.pypi.org/simple/ pycairo
+./python.$PYVER/tools/python.exe -m pip install -r manim/requirements.txt
+curl -L https://ffmpeg.zeranoe.com/builds/win64/static/$FFMPEG_FILE.zip -o $FFMPEG_FILE.zip
+7z x $FFMPEG_FILE.zip -oFFmpeg/
+cd FFmpeg && cp -r $FFMPEG_FILE/* . && rm -r $FFMPEG_FILE && cd ../
+curl -L http://downloads.sourceforge.net/project/sox/sox/14.4.2/$SOX_FILE.zip -o $SOX_FILE.zip
+7z x $SOX_FILE.zip -oSoX/
+cd SoX && cp -r $SOX_FILE/* . && rm -r $SOX_FILE && cd ../
+cd ../
