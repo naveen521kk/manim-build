da23c327a1facd705fde21e2c975353b5eb22214
Naveen M K <naveen@syrusdark.website>
2020-07-21 09:13:54 +0000

    Add the basic Install (#2)
    * Dont use Pyinstaller
    
    * Fix build
    
    * Fix build
    
    * Fix build  1
    
    * fix build 2
    
    * Try for 64 bit
    
    * Try again-Fix path
    
    * Try pushing
    
    * Try again
    
    * Remove dependencies
    
    * Try Again

---
+++ python-x86.sh
@@ -0,0 +1,7 @@
+# build x86 python
+
+export FFMPEG_FILE=ffmpeg-4.3-win32-static
+export SOX_FILE=sox-14.4.2-win32
+./pythonx86.$PYVER/tools/python.exe -m pip install -i https://test.pypi.org/simple/ pycairo
+./pythonx86.$PYVER/tools/python.exe -m pip install -r manim/requirements.txt
+cd ../
