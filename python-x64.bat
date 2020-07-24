0586a627878c031cc8084ef260299e6a856cccf7
Syrus Dark <naveen@syrusdark.website>
2020-07-24 16:40:35 +0530

    Remove Python

--- python-x64.bat
+++ python-x64.bat
@@ -2,6 +2,6 @@
 python.%PYVER%\tools\python.exe -m pip install -i https://test.pypi.org/simple/ pycairo
 cd manim
 ..\python.%PYVER%\tools\python.exe -m pip install .
-cd ../
-dir python.%PYVER%/tools/Scripts
-cd ../
+cd ..\
+dir python.%PYVER%\tools\Scripts
+cd ..\
