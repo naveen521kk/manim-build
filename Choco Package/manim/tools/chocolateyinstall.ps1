55e653052959bfa1df4dc20d746a5d3b7751c038
Syrus Dark <naveen@syrusdark.website>
2020-07-28 16:23:37 +0530

    Try a small fix

--- Choco Package/manim/tools/chocolateyinstall.ps1
+++ Choco Package/manim/tools/chocolateyinstall.ps1
@@ -31,9 +31,9 @@ else {
     dir
     ./python.exe -m pip install "$toolsDir\python.3.8.3.x64\manim"
     #python -m pip install $manimdir
-    cd $oridir
+    #cd $oridir
     #dir $ChocolateyInstall
-    $files = get-childitem $installDir -include *.exe -recurse
+    $files = get-childitem $toolsDir -include *.exe -recurse
     foreach ($file in $files) {
       if (!($file -eq "manim.exe" -or $file -eq "manimcm.exe")){
         New-Item "$file.ignore" -type file -force | Out-Null
