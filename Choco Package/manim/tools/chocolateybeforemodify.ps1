6392c741c1da1b0b5f75788c4973529dddb3bdb8
Syrus Dark <naveen@syrusdark.website>
2020-07-28 20:04:32 +0530

    Try update thing

--- Choco Package/manim/tools/chocolateybeforemodify.ps1
+++ Choco Package/manim/tools/chocolateybeforemodify.ps1
@@ -1,4 +1,4 @@
-﻿$pydir= "$toolsDir\python.3.8.3.x64\python.3.8.3\tools"
+﻿$pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
 Set-Location $pydir
 ./python.exe -m pip install -q --upgrade pip
 ./python.exe -m pip install -q https://github.com/ManimCommunity/manim/archive/master.zip
