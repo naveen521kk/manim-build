249e2c71f6eb4cd343b98c0f38d95c1ba2024914
Syrus Dark <naveen@syrusdark.website>
2020-07-28 20:23:18 +0530

    Try upgrade test

--- Choco Package/manim/tools/chocolateyuninstall.ps1
+++ Choco Package/manim/tools/chocolateyuninstall.ps1
@@ -1,9 +1,4 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $osBitness = Get-ProcessorBits
 if ( $osBitness -eq 64 ) {
   Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName "build.3.8.3.x64.exe"
