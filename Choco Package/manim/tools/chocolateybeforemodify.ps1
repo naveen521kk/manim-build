7cd1af3403c1207efc5ffe3afddfbf8f10ec48d8
Naveen M K <naveen@syrusdark.website>
2020-07-29 10:20:26 +0530

    Update Try catch block

--- Choco Package/manim/tools/chocolateybeforemodify.ps1
+++ Choco Package/manim/tools/chocolateybeforemodify.ps1
@@ -2,9 +2,16 @@
 $osBitness = Get-ProcessorBits
 $pydirx86= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x86\pythonx86.3.8.3\tools"
 $pydirx64= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
-Try { Set-Location $pydirx86}
-Catch { Set-Location $pydirx64}
-         
+Try { 
+Set-Location $pydirx86
 ./python.exe -m pip install -q --upgrade pip
 ./python.exe -m pip install -q https://github.com/ManimCommunity/manim/archive/master.zip
 Write-Output "Manim Updated to Master Branch"
+}
+Catch { 
+Set-Location $pydirx64
+./python.exe -m pip install -q --upgrade pip
+./python.exe -m pip install -q https://github.com/ManimCommunity/manim/archive/master.zip
+Write-Output "Manim Updated to Master Branch"}
+         
+
