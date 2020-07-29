a240b5df33743ac50c1d59c296bfae14dc084c60
Naveen M K <naveen@syrusdark.website>
2020-07-29 10:07:40 +0530

    Update chocolateybeforemodify.ps1

--- Choco Package/manim/tools/chocolateybeforemodify.ps1
+++ Choco Package/manim/tools/chocolateybeforemodify.ps1
@@ -1,11 +1,10 @@
 
 $osBitness = Get-ProcessorBits
-if ( $osBitness -eq 32 -or $env:ChocolateyForceX86 -eq "true") {
-  $pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x86\pythonx86.3.8.3\tools"
-} else {
-  $pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
-}
-Set-Location $pydir
+$pydirx86= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x86\pythonx86.3.8.3\tools"
+$pydirx64= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
+Try { Set-Location $pydirx86}
+Catch { Set-Location $pydirx64}
+         
 ./python.exe -m pip install -q --upgrade pip
 ./python.exe -m pip install -q https://github.com/ManimCommunity/manim/archive/master.zip
 Write-Output "Manim Updated to Master Branch"
