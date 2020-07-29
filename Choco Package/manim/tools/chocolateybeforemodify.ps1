95ff4caa1e685d3761019556ea19f6ca9f95f76a
Naveen M K <naveen@syrusdark.website>
2020-07-29 09:40:16 +0530

    Fix update script

--- Choco Package/manim/tools/chocolateybeforemodify.ps1
+++ Choco Package/manim/tools/chocolateybeforemodify.ps1
@@ -1,4 +1,10 @@
-﻿$pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
+
+$osBitness = Get-ProcessorBits
+if ( $osBitness -eq 32 -or $env:ChocolateyForceX86 -eq "true") {
+  $pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x86\pythonx86.3.8.3\tools"
+else {
+  $pydir= "$env:ChocolateyPackageFolder\tools\python.3.8.3.x64\python.3.8.3\tools"
+}
 Set-Location $pydir
 ./python.exe -m pip install -q --upgrade pip
 ./python.exe -m pip install -q https://github.com/ManimCommunity/manim/archive/master.zip
