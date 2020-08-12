0b91200d841aaf11cbc49069596a93906431ef3c
Naveen M K <naveen@syrusdark.website>
2020-08-12 14:47:10 +0530

    Slight update

--- Choco Package/manim.install/tools/chocolateyinstall.ps1
+++ Choco Package/manim.install/tools/chocolateyinstall.ps1
@@ -1,11 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$manimExtractDir=Join-Path $toolsDir "manim"
 python -m pip install -i https://test.pypi.org/simple/ -q pycairo
-
-Get-ChocolateyWebFile `
-  -PackageName "manim" `
-  -FileFullPath "$toolsDir\manim.zip" `
-  -Url "https://github.com/ManimCommunity/manim/archive/master.zip"
-  
-python -m pip install -q $manimExtractDir
+python -m pip install -q https://github.com/manimcommunity/manim/archive/master.zip
