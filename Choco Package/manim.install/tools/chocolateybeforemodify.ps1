cb883a420be0a51b955855751edab90973ddd2d5
Syrus Dark <naveen@syrusdark.website>
2020-08-13 20:44:36 +0530

    Remove Comments and update Few lines in nuspec file

--- Choco Package/manim.install/tools/chocolateybeforemodify.ps1
+++ Choco Package/manim.install/tools/chocolateybeforemodify.ps1
@@ -1,8 +1,6 @@
 ﻿$manim = Get-Process manim -ErrorAction SilentlyContinue
 if ($manim) {
-  # try gracefully first
   $manim.CloseMainWindow()
-  # kill after five seconds
   Sleep 5
   if (!$manim.HasExited) {
     $manim | Stop-Process -Force
